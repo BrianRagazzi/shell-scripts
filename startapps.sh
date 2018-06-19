2cbb5fa9b6c17aa123754d6574461446f20443d2
Brian Ragazzi <brianpragazzi@gmail.com>
2018-06-19 17:34:43 -0500

    setup s3cmd and write out config

--- startapps.sh
+++ startapps.sh
@@ -1,2 +1,26 @@
 cd minio
 docker-compose up -d
+
+wget https://github.com/s3tools/s3cmd/archive/master.zip
+unzip master.zip
+sudo python setup.py install
+
+
+cat > ~/.s3cfg  <<'EOF'
+# Setup endpoint
+host_base = localhost:9000
+host_bucket = localhost:9000
+bucket_location = us-east-1
+use_https = False
+
+# Setup access keys
+access_key =  MYACCESSKEY
+secret_key = MYSECRETKEY
+
+# Enable S3 v4 signature APIs
+signature_v2 = False
+EOF
+
+
+
+s3cmd mb s3://binaries
