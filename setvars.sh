072bb0dcba4403454ebfbf9d228d8ce1b6e3e2f7
Brian Ragazzi <brianpragazzi@gmail.com>
2018-08-23 10:00:53 -0500

    specify default creds

---
+++ setvars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#Update this file with the desired credentials
+
+#Credentials used in minio
+ACCESS_KEY=MYACCESSKEY
+SECRET_KEY=MYSECRETKEY
+
+#Credentials used in Concourse
+CONCOURSE_BASIC_AUTH_USERNAME=concourse
+CONCOURSE_BASIC_AUTH_PASSWORD=changeme
