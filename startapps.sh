bab4023faa903c86205a9a00d93e448561cd432f
Brian Ragazzi <brianpragazzi@gmail.com>
2018-08-23 13:52:40 -0500

    show correct credentials

--- startapps.sh
+++ startapps.sh
@@ -42,5 +42,5 @@ cd $CURRDIR/concourse
 
 docker-compose up -d
 
-echo "Connect to concourse using http://$HOST_IP, logon as concourse|changeme"
-echo "Connect to minio using http://$HOST_IP:9000, logon as MYACCESSKEY|MYSECRETKEY"
+echo "Connect to concourse using http://$HOST_IP, logon as $CONCOURSE_BASIC_AUTH_USERNAME|$CONCOURSE_BASIC_AUTH_PASSWORD"
+echo "Connect to minio using http://$HOST_IP:9000, logon as $MINIO_ACCESS_KEY|$MINIO_SECRET_KEY
