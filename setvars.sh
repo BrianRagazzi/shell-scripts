570d025fe1ba17d3292b19a3f6279cccaace666c
Brian Ragazzi <brianpragazzi@gmail.com>
2018-08-23 13:42:09 -0500

    export vars

--- setvars.sh
+++ setvars.sh
@@ -3,9 +3,12 @@
 #Update this file with the desired credentials
 
 #Credentials used in minio
-ACCESS_KEY=MYACCESSKEY
-SECRET_KEY=MYSECRETKEY
+export ACCESS_KEY=MYACCESSKEY
+export SECRET_KEY=MYSECRETKEY
 
 #Credentials used in Concourse
-CONCOURSE_BASIC_AUTH_USERNAME=concourse
-CONCOURSE_BASIC_AUTH_PASSWORD=changeme
+export CONCOURSE_BASIC_AUTH_USERNAME=concourse
+export CONCOURSE_BASIC_AUTH_PASSWORD=changeme
+
+HOST_IP=$(hostname -I | cut -d' ' -f 1)
+export CONCOURSE_EXTERNAL_URL=$HOST_IP
